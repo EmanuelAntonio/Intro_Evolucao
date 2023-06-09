582fc9b573cf40c8c8665561ba7c13ee016be263
Leon Sulfierry <leon.bio0@hotmail.com>
2023-06-08 21:21:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,38 +6,68 @@ from implementacao05 import *
 from implementacao06 import *
 import os
 
+# 1º implementação: Mutação recorrente unidirecional
 # if not os.path.exists("./implementacao01"):
 #     os.mkdir("./implementacao01")
 # implementacao01()
-#
+
+# 1.1º implementação: Mutação recorrente unidirecional com população finita
 # if not os.path.exists("./implementacao01_Finita"):
 #     os.mkdir("./implementacao01_Finita")
 # implementacao01_Finita()
-#
+
+
+# 2º implementação: Mutação recorrente bidirecional
 # if not os.path.exists("./implementacao02"):
 #     os.mkdir("./implementacao02")  # problema
 # implementacao02()
-#
+
+# 2.1º implementação: Mutação recorrente bidirecional com populaçao finita
 # if not os.path.exists("./implementacao02_Finita"):
 #     os.mkdir("./implementacao02_Finita")
 # implementacao02_Finita()
 
-if not os.path.exists("./implementacao03"):
-    os.mkdir("./implementacao03")
-# implementacao3()
-# implementacao32()
-# implementacao33()
-# implementacao34()
-implementacao35()
+# 3º implementação: Os macacos de Shakespeare
+#if not os.path.exists("./implementacao03"):
+#    os.mkdir("./implementacao03")
+#implementacao3()
 
-# if not os.path.exists("./implementacao04"):
+# 3.1° implementação: Os macacos de Shakespeare com mutação
+#if not os.path.exists("./implementacao03"):
+#    os.mkdir("./implementacao03")
+#implementacao31()
+
+# 3.2º implementação: Expandindo os macacos de Shakespeare (população, mutação, seleção)
+#if not os.path.exists("./implementacao03"):
+#    os.mkdir("./implementacao03")
+#implementacao32()
+
+# 3.3° implementação: recombinação
+#if not os.path.exists("./implementacao03"):
+#    os.mkdir("./implementacao03")
+#implementacao33()
+
+# 3.4° implementação: Deriva gênica
+#if not os.path.exists("./implementacao03"):
+#    os.mkdir("./implementacao03")
+#implementacao34()
+
+# 3.5° implementação: migração
+#if not os.path.exists("./implementacao03"):
+#    os.mkdir("./implementacao03/3.5")
+#implementacao35()
+
+# 4º Implementação: mModelo de um loco
+#if not os.path.exists("./implementacao04"):
 #     os.mkdir("./implementacao04")
-# implementacao04()
+#implementacao04()
 
-# if not os.path.exists("./implementacao05"):
-    #os.mkdir("./implementacao05")
+# 5º implementação: Deriva gênica
+#if not os.path.exists("./implementacao05"):
+#    os.mkdir("./implementacao05")
 #implementacao05()
 
-# if not os.path.exists("./implementacao06"):
+# 6º implementação: Mutação, deriva e seleção natural
+#if not os.path.exists("./implementacao06"):
 #     os.mkdir("./implementacao06")
-# implementacao06()
+#implementacao06()
